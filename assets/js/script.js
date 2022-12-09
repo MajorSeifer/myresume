30a2afd6e53f9f3d1524497387dca9b1f73f7844
Jonathan Manes <manesjona@gmail.com>
2022-12-09 10:34:52 +0100

    dar-kitchen

---
+++ assets/js/script.js
@@ -0,0 +1,209 @@
+import { DISHES } from "./config.js";
+import { addToCart, calculateTotal, clearCart, showCart } from "./shopping-cart.js";
+
+let dayNightButton;
+
+function main() {
+    let dishes = DISHES;
+    let main = document.querySelector("main");
+    let section = document.createElement("section");
+    section.id = "menu-tartines"
+    let div = document.createElement("div");
+    div.className = "title-main";
+    let title = document.createElement("h2");
+    title.innerText = "Nos tartines";
+    div.appendChild(title);
+
+    for (let dish of dishes) {
+        section.appendChild(drawDishes(dish));
+    }
+    let shoppingCartButton = drawShoppingCartButton();
+
+    main.appendChild(div);
+    main.appendChild(shoppingCartButton);
+    main.appendChild(section);
+    document.body.insertBefore(main, document.querySelector("footer"));
+    drawShoppingCart();
+    drawDayNightSwitch();
+    drawResetSwitch();
+    drawSwitchVegan();
+}
+
+function drawDayNightSwitch() {
+    // Day-Night switch
+    dayNightButton = document.createElement("button");
+    dayNightButton.className = "day-night"
+    let dayNightIcon = document.createElement("i");
+    dayNightIcon.setAttribute("class", "fas fa-moon");
+    dayNightButton.appendChild(dayNightIcon);
+    document.body.insertBefore(dayNightButton, document.querySelector("footer"));
+
+    dayNightButton.addEventListener("click", () => {
+        if (document.body.className != "dark") {
+            localStorage.setItem("theme", "dark");
+            dayNightButton.querySelector("i").className = "fa fa-sun";
+            document.body.className = "dark";
+        } else {
+            dayNightButton.querySelector("i").className = "fa fa-moon";
+            localStorage.setItem("theme", "light")
+            document.body.className = ""
+        }
+    });
+}
+
+function drawSwitchVegan() {
+    let switchVegan = document.createElement("button");
+    switchVegan.innerText = "Plats végétaliens";
+
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fa-solid fa-seedling");
+    switchVegan.prepend(icon);
+
+    let div = document.querySelector(".title-main");
+    div.appendChild(switchVegan);
+    switchVegan.addEventListener("click", () => {
+        let articles = document.querySelectorAll("article")
+
+        articles.forEach(element => {
+            if (!element.classList.contains("vegan")) {
+                element.style.display = "none";
+            }
+        });
+    });
+}
+
+function drawResetSwitch() {
+    let switchReset = document.createElement("button");
+    switchReset.innerText = "Tous les plats";
+
+    let div = document.querySelector(".title-main");
+    div.appendChild(switchReset);
+
+    switchReset.addEventListener("click", () => {
+        let articles = document.querySelectorAll("article")
+
+        articles.forEach(element => {
+            element.style.display = "block";
+
+        });
+    });
+}
+
+function drawShoppingCart() {
+    let aside = document.createElement("aside");
+    aside.className = "shopping-cart";
+    aside.style.display = "none";
+
+    let ol = document.createElement("ol");
+    ol.className = "shopping-list";
+    aside.appendChild(ol);
+
+    let resetButton = document.createElement("button");
+    resetButton.innerText = "Reset the cart";
+    resetButton.addEventListener("click", () => {
+        clearCart();
+    })
+
+    aside.appendChild(resetButton);
+
+    let total = document.createElement("h3");
+    total.className = "total";
+    aside.appendChild(total);
+
+    let main = document.querySelector("main");
+    main.appendChild(aside);
+}
+
+/**
+ * Create the shopping cart element
+ * @returns the shopping cart button
+ */
+function drawShoppingCartButton() {
+    let cartButton = document.createElement("button");
+    cartButton.className = "cart-button"
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-shopping-cart");
+    cartButton.appendChild(icon);
+
+    cartButton.addEventListener("click", (e) => {
+        let aside = document.querySelector("aside");
+        if (aside.style.display === "none") {
+            showCart();
+        } else {
+            aside.style.display = "none";
+        }
+    })
+    return cartButton;
+}
+
+/**
+ * Create an article for each dishe
+ * @param {Object} dishe 
+ * @returns the article
+ */
+function drawDishes(dishe) {
+    let article = document.createElement("article");
+
+    // Create the picture
+    let picture = document.createElement("img");
+    picture.setAttribute("alt", dishe.name);
+    picture.setAttribute("src", dishe.picture);
+    article.appendChild(picture);
+
+    // Name of the dish
+    let title = document.createElement("h3");
+    title.innerText = dishe.name;
+    article.appendChild(title);
+
+    // Price of the dish
+    let price = document.createElement("h4");
+    price.innerText = dishe.price + " €";
+    article.appendChild(price);
+
+    // Create the inner section
+    let innerSection = document.createElement("section");
+    innerSection.className = "inner-section";
+
+    // Add dish to shopping cart
+    let addToCartButton = document.createElement("button");
+    addToCartButton.innerText = "Ajouter au panier"
+    addToCartButton.addEventListener("click", () => {
+        addToCart(dishe);
+    })
+    innerSection.appendChild(addToCartButton);
+
+    // Create summary
+    let summary = document.createElement("p");
+    summary.innerText = dishe.description;
+    innerSection.appendChild(summary);
+
+    // Vegan pin
+    if (dishe.category.includes("vegan")) {
+        let icon = document.createElement("i");
+        icon.setAttribute("class", "fa-solid fa-seedling");
+        innerSection.appendChild(icon);
+        article.classList.toggle("vegan");
+    }
+
+    // Create the back section
+    let backSection = document.createElement("section");
+    backSection.className = "back-section";
+
+    // Assemble
+    article.appendChild(innerSection);
+    article.appendChild(backSection);
+
+    return article;
+}
+
+
+export function responsiveMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+main();
---
+++ assets/js/shopping-cart.js
@@ -0,0 +1,79 @@
+let orderListEmpty = [];
+
+/**
+ * Store the object in sessionStorage and display a confirmation window
+ * @param {Object} dishe 
+ */
+export function addToCart(dishe) {
+    orderListEmpty.push(dishe);
+    sessionStorage.setItem("shopping-cart", JSON.stringify(orderListEmpty));
+    showCart();
+    calculateTotal();
+}
+
+/**
+ * Display the shopping cart and create an item the shopping list
+ */
+export function showCart() {
+    let aside = document.querySelector("aside");
+    let ol = document.querySelector(".shopping-list");
+    ol.innerHTML = null;
+
+    let orderList = orderListEmpty;
+    if (orderList !== null) {
+        for (let dishe of orderList) {
+            let li = document.createElement("li");
+            li.innerText = dishe.name;
+            ol.appendChild(li);
+        }
+        aside.appendChild(ol);
+        document.body.appendChild(aside);
+    }
+
+    let total = document.querySelector(".total");
+    total.innerText = calculateTotal()
+
+
+    aside.style.display = "flex";
+    ol.style.display = "block";
+}
+
+
+/**
+ * Set the shopping cart list to null and remove content from the list
+ */
+export function clearCart() {
+    orderListEmpty = [];
+    let ol = document.querySelector(".shopping-list");
+    ol.innerHTML = null;
+    sessionStorage.setItem("shopping-cart", JSON.stringify(orderListEmpty));
+    calculateTotal();
+    let total = document.querySelector(".total");
+    total.innerText = "0,00 €";
+}
+
+
+/**
+ * Take the price of each dishe in the shopping cart and add
+ */
+export function calculateTotal() {
+    let orderList = JSON.parse(sessionStorage.getItem("shopping-cart"));
+    if (orderList !== null) {
+        let total = 0.00;
+        for (let dishe of orderList) {
+            total = (total + dishe.price);
+        }
+        return formatter.format(total) + " €";
+    } else {
+        return "0,00 €"
+    }
+}
+
+
+/**
+ * A formatter function to round number at 2 decimals
+ */
+const formatter = new Intl.NumberFormat('fr-FR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
